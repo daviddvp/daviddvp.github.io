a0feaada0687efa775c9beec7f182fa31a3ba5a8
David Del Valle Puado <dvallepuado@gmail.com>
2026-05-01 21:14:50 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/daviddvp.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
